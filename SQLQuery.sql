0ef8f61bbb9723ff10a84ea30b3a6b3a8c23cfb2
camilacanaviri <camicmz.77@gmail.com>
2022-12-16 23:30:50 -0400

    Subir

---
+++ SQLQuery.sql
@@ -0,0 +1,52 @@
+USE [EmpresaVelmotor]
+
+
+SELECT * FROM Servicio 
+INSERT INTO Servicio  VALUES ('MAT.GEN.-1','Mantenimietno General ','Chequeo y cambio de fluidos y/o niveles ',50)
+INSERT INTO Servicio  VALUES ('MAT.GEN.-2','Mantenimietno General ','Limpieza y cambio de filtros de aire, aceite, gasolina.',50)
+INSERT INTO Servicio VALUES ('MAT.GEN.-3','Mantenimietno General ','Reglaje de frenos en las 4 ruedas y freno de mano ',50)
+INSERT INTO Servicio VALUES ('MAT.GEN.-4','Mantenimietno General ','Lavado y Engrase: Motor - Chasis ','80')
+INSERT INTO Servicio VALUES ('MAT.GEN.-5','Mantenimietno General ','Lavado  y Limpieza General exterior e interior ',70)
+INSERT INTO Servicio VALUES ('MAT.GEN.-6','Mantenimietno General ','Lavado Especial exterior e interior ','140')
+INSERT INTO Servicio VALUES ('MAT.GEN.-7','Mantenimietno General ','Retiro - Colocado de logos empresariales (ambas puertas) ',50)
+INSERT INTO Servicio VALUES ('MAT.GEN.-8','Mantenimietno General ','Alineado de Ruedas ',70)
+INSERT INTO Servicio VALUES ('MAT.GEN.-9','Mantenimietno General ','Balanceo de Ruedas ','70')
+INSERT INTO Servicio VALUES ('MAT.GEN.-10','Mantenimietno General ','Parchado de Llantas con cámara y sobre aro ',20)
+INSERT INTO Servicio VALUES ('MAT.GEN.-11','Mantenimietno General ','Parchado de llantas radial-tubular ',20)
+INSERT INTO Servicio VALUES ('MAT.GEN.-12','Mantenimietno General ','Vulcanizado de Llantas',20)
+
+
+INSERT INTO Servicio VALUES ('MOTOR.-1','Motor','Afinado de motor (Carburador)',50)
+INSERT INTO Servicio VALUES ('MOTOR.-2','Motor','Afinado de motor  (Inyectores)',350)
+INSERT INTO Servicio VALUES ('MOTOR.-3','Motor','Limpieza de Bomba Inyectora','100')
+INSERT INTO Servicio VALUES ('MOTOR.-4','Motor','Limpieza con ultrasonido de inyectores c/u',50)
+INSERT INTO Servicio VALUES ('MOTOR.-5','Motor','Banco de prueba (caudal y estanqueidad de inyectores)',150)
+INSERT INTO Servicio VALUES ('MOTOR.-6','Escaneado del sistema electrónico de inyección',120)
+INSERT INTO Servicio VALUES ('MOTOR.-7','Regulado de Válvulas','50')
+INSERT INTO Servicio VALUES ('MOTOR.-8','Motor','Limpieza de Tanque de Combustible',150)
+INSERT INTO Servicio VALUES ('MOTOR.-9','Motor','Limpieza de Radiador y sistema de enfriamiento',100)
+INSERT INTO Servicio VALUES ('MOTOR.-10','Motor','Revisión de Sensores','100')
+INSERT INTO Servicio VALUES ('MOTOR.-11','Motor','Cambio de Correa de Distribución dentada y tensores',300)
+INSERT INTO Servicio VALUES ('MOTOR.-12','Motor','Cambio de Bomba de Agua',100)
+INSERT INTO Servicio VALUES ('MOTOR.-13','Motor','Cambio de Bomba de Gasolina',150)
+INSERT INTO Servicio VALUES ('MOTOR.-14','Motor','Desmontado de Motor','1000')
+INSERT INTO Servicio VALUES ('MOTOR.-15','Motor','Cambio de soporte de Motor','100')
+INSERT INTO Servicio VALUES ('MOTOR.-16','Motor','Revisión, Verificación y/o cambio de piezas ',150)
+INSERT INTO Servicio VALUES ('MOTOR.-17','Motor','Revisión de Fugas de Aceite y Fluidos',30)
+INSERT INTO Servicio VALUES ('MOTOR.-18','Motor','Montaje de Motor hasta su Encendido',1300)
+
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-1','SISTEMA DE DIRECCION ','Revisión y Diagnóstico de la Bomba de Dirección, Líquido del Sistema',50)
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-2','SISTEMA DE DIRECCION ','Cambio de Muñones de Dirección ','50')
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-3','SISTEMA DE DIRECCION ','Revisión, Diagnóstico y Cambio de la Caja de Dirección, Cremallera',50)
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-4','SISTEMA DE DIRECCION ','Cambio del brazo Pitman de dirección',50)
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-5','SISTEMA DE DIRECCION ','Cambio del brazo de apoyo de dirección',50)
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-6','SISTEMA DE DIRECCION ','Reparación de la columna de dirección ',50)
+INSERT INTO Servicio VALUES ('SIST.DE.DIREC.-7','SISTEMA DE DIRECCION ','Cambio de la cremallera hidráulica de dirección ',50)
+SELECT *FROM Servicio
+
+ 
+
+
+GO
+
+
---
+++ SQLQuery1.sql
@@ -0,0 +1,43 @@
+USE [EmpresaVelmotor]
+GO
+--insertar
+INSERT INTO [dbo].[Cliente]  VALUES ('1001','Fredy ','Daza','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1002',' Rafael','Aviles','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1003','Rodrigo ','Grajeada','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1004','Marcelo','Cerezo','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1005','Alvaro','Gardeazabal','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1006','Marcelo ','Bellido','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1007','Paulo ','Vallejos','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+INSERT INTO [dbo].[Cliente]  VALUES ('1008','Wilver ','Alaca','Calle España No 2171','46910069',' ENTEL S.A.REGIONAL CHUQUISACA')
+select *from Cliente
+--actualizar
+
+
+INSERT INTO [dbo].[usuario] VALUES ('1001','Fredy ','1234')
+INSERT INTO [dbo].[usuario] VALUES ('1001','Rafael ','567')
+INSERT INTO [dbo].[usuario] VALUES ('1002','Rodrigo ','890')
+INSERT INTO [dbo].[usuario] VALUES ('1003','Marcelo','5689')
+INSERT INTO [dbo].[usuario] VALUES ('1004','Alvaro','8956')
+INSERT INTO [dbo].[usuario] VALUES ('1005','Marcelo ','2585')
+INSERT INTO [dbo].[usuario] VALUES ('1006','Paulo','9899')
+INSERT INTO [dbo].[usuario] VALUES ('1007','Wilver ','8888')
+INSERT INTO [dbo].[usuario] VALUES ('2001','Patricio ','1589')
+INSERT INTO [dbo].[usuario] VALUES ('2002','Luis','1555')
+INSERT INTO [dbo].[usuario] VALUES ('2003','Patricio ','7777')
+
+
+select *from usuario
+--actualizar
+
+INSERT INTO [dbo].[Empleado] VALUES ('EM01','2001','Patricio','tecnico','si')
+INSERT INTO [dbo].[Empleado] VALUES ('EM02','2002','Luis','Gerente','si')
+INSERT INTO [dbo].[Empleado] VALUES ('EM03','2003','Patricio','Limpieza','si')
+
+select *from Empleado
+
+
+
+
+
+GO
+
